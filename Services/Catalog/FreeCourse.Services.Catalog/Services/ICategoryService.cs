07feacd921ea04111720338c39b71e8f3f4bf213
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 00:20:31 +0300

    Category service interface, class added

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> GetById(string id);
+    }
+}
