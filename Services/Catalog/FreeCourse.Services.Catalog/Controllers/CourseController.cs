022b4bf8afbaee37191e6bbeebab806ac1cdf1ea
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 02:26:05 +0300

    CourseController GetById ActionResult Added

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CourseController : ControllerBase
+    public class CourseController : CustomBaseController
     {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response=await _courseService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+            
+        }
     }
 }
