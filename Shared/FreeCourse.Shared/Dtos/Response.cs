0cc79ba1e0d79f731ccedbe754e4d3e3baa6c2ac
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 00:20:42 +0300

    ResponseDto renamed as Response

---
+++ Shared/FreeCourse.Shared/Dtos/Response.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class Response<T>
+    {
+        //static factory Method
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccessful = false };
+        }
+
+        public static Response<T> Fail(List<string> errors, int statuscode)
+        {
+            return new Response<T>
+            {
+                Errors = errors,
+                StatusCode = statuscode,
+                IsSuccessful = false
+            };
+        }
+
+        public static Response<T> Fail(string error, int statusCode)
+        {
+            return new Response<T> { Errors = new List<string> { error }, StatusCode = statusCode, IsSuccessful = false };
+        }
+
+    }
+}
