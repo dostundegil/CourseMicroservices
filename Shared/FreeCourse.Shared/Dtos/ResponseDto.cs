e92c1ac6a4de458b7db19fd7bde6078b26def03d
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-17 23:40:55 +0300

    Add project files.

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        //static factory Method
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode, IsSuccessful = false };
+        }
+
+        public static ResponseDto<T> Fail(List<string> errors,int statuscode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                StatusCode = statuscode,
+                IsSuccessful = false
+            };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode)
+        {
+            return new ResponseDto<T> { Errors = new List<string> { error }, StatusCode = statusCode, IsSuccessful = false };
+    }
+
+}
