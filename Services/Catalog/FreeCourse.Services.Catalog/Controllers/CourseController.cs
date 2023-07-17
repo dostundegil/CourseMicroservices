08c247d0d1f72b0653a325619cdd7c70339933ff
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 02:29:23 +0300

    Added GetAllByUserId ActionResult to CourseController

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -16,12 +16,27 @@ namespace FreeCourse.Services.Catalog.Controllers
         {
             _courseService = courseService;
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
 
+        }
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
             var response=await _courseService.GetByIdAsync(id);
             return CreateActionResultInstance(response);
             
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserId(userId);
+            return CreateActionResultInstance(response);
+
         }
     }
 }
