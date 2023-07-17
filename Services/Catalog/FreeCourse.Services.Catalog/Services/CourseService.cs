0abaa175dce02f2f5a46b6031b685fcc1b8cecd9
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 01:53:11 +0300

    added GetAll method

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -1,6 +1,47 @@
-﻿namespace FreeCourse.Services.Catalog.Services
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
 {
     public class CourseService
     {
+        private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
+
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
     }
 }
