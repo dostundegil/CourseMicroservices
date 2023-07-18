43f541cf37970c5e97e556f8d438dc371690e038
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 16:43:15 +0300

    SignUp method added to UserController

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        [HttpPost]
+
+        public async Task<IActionResult> SingUp(SignUpDto signUp)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUp.Username,
+                Email = signUp.Email,
+                City = signUp.City,
+            };
+            var result = await _userManager.CreateAsync(user,signUp.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
+            }
+
+            return NoContent();
+
+        }
+    }
+}
