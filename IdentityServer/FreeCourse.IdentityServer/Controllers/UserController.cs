c126c20da54c9efb79d9bb3274e4471af73af17f
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 22:56:51 +0300

    UserController Updated

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -29,7 +29,7 @@ namespace FreeCourse.IdentityServer.Controllers
 
             if(!result.Succeeded)
             {
-                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400)); 
             }
 
             return NoContent();
