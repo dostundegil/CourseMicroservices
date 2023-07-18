9256aa8cf38e1e1250dff69da861cb7cdef150bb
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-19 01:55:19 +0300

    Added authorize to UserController

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -1,14 +1,18 @@
 ﻿using FreeCourse.IdentityServer.Dtos;
 using FreeCourse.IdentityServer.Models;
 using FreeCourse.Shared.Dtos;
+using IdentityServer4.Hosting.LocalApiAuthentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace FreeCourse.IdentityServer.Controllers
 {
+    [Authorize(LocalApi.PolicyName)]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
