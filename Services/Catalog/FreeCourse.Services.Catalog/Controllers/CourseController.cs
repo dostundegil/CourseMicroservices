edf7e33970e95186241be9a8c987811e75802274
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-18 13:40:59 +0300

    CourseController Updated

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -34,6 +34,7 @@ namespace FreeCourse.Services.Catalog.Controllers
 
         }
 
+        [HttpGet]
         [Route("/api/[controller]/GetAllByUserId/{userId}")]
         public async Task<IActionResult> GetAllByUserId(string userId)
         {
