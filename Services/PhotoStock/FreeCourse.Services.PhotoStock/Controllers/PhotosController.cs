4e34518300b5fb4a06dea945c5ac87570716bb5c
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-19 23:47:45 +0300

    Added PhotoDelete > PhotosController

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -34,5 +34,16 @@ namespace FreeCourse.Services.PhotoStock.Controllers
 
             return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
         }
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if(!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
     }
 }
