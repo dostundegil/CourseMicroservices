c73aafbe38046ce1bab44daf82d4d92d449a43be
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-20 17:40:33 +0300

    Discount > DiscountsController created

---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+    }
+}
