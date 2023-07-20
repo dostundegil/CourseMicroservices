f58499fb0ba60cda4064fc691ec100e13f435a44
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-20 17:42:45 +0300

    Discount > Startup updated

--- Services/Discount/FreeCourse.Services.Discount/Startup.cs
+++ Services/Discount/FreeCourse.Services.Discount/Startup.cs
@@ -1,3 +1,5 @@
+using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -31,7 +33,8 @@ namespace FreeCourse.Services.Discount
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpContextAccessor();
-
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddScoped<IDiscountService, DiscountService>();
             var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
