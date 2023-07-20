29c1a8145974aafc031c59093d49a0dfc24d2874
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-20 03:16:24 +0300

    Shared > SharedIdentityService updated

--- Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FreeCourse.Shared.Services
 {
-    internal class SharedIdentityService
+    public class SharedIdentityService : ISharedIdentityService
     {
+        private IHttpContextAccessor _contextAccessor;
+
+        public SharedIdentityService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public string GetUserId => _contextAccessor.HttpContext.User.FindFirst("sub").Value;
     }
+
+
 }
