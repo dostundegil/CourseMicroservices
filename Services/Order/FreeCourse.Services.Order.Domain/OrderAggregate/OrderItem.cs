07a820cbf9012fdc588567231b9544d8a1598e67
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-22 23:24:58 +0300

    Domain > OrderItem updated

--- Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.Domain.OrderAggregate
 {
-    internal class OrderItem
+    public class OrderItem
     {
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public void UpdateOrderItem(string productName , string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
     }
 }
