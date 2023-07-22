19dd983b79a6d6f91c100d3a01cfaa16c1c02032
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-22 09:34:37 +0300

    Domain.Core > ValueObject updated

--- Services/Order/FreeCourse.Services.Order.Domain.Core/ValueObject.cs
+++ Services/Order/FreeCourse.Services.Order.Domain.Core/ValueObject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.Domain.Core
 {
-    public class ValueObject
+    public abstract class ValueObject
     {
         protected static bool EqualOperator(ValueObject left, ValueObject right)
         {
