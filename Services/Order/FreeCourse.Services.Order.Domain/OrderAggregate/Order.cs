e4779cb778e01ee7634946ffa2713779eed06103
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-23 00:35:38 +0300

    Domain > Order updated

--- Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.Domain.OrderAggregate
 {
-    public class Order
+    public class Order : Entity, IAggregateRoot
     {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+        private readonly List<OrderItem> _orderItems;
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
     }
 }
