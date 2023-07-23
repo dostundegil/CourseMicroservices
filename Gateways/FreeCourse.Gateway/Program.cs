addcc10fb5c5653bfd51adbe396d611c3262673c
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-23 16:04:25 +0300

    configuration json files updated

--- Gateways/FreeCourse.Gateway/Program.cs
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -17,7 +17,10 @@ namespace FreeCourse.Gateway
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext,config)=>
+            {
+                config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
