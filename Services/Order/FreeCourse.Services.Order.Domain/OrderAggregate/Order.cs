4e9df68f4a1b6e5806e2518e7b8103e65bb7f1a5
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-23 03:27:56 +0300

    Order > Domain updated

--- Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -16,6 +16,11 @@ namespace FreeCourse.Services.Order.Domain.OrderAggregate
 
         public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
 
+        public Order()
+        {
+            
+        }
+
         public Order(string buyerId, Address address)
         {
             _orderItems = new List<OrderItem>();
