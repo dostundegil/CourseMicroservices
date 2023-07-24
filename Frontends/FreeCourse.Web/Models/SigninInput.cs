9c46b7f0a88231f9895c2ec6e30eea6bc44a905d
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-24 16:11:55 +0300

    IIdentityService / IdentityModel library added

---
+++ Frontends/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name ="Email adresiniz")]
+        public string Email { get; set; }
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember{ get; set; }
+    }
+}
