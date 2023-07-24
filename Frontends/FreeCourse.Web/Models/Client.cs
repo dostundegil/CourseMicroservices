15173532afae4c7074263e8984db0d69b76e455d
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-24 16:32:35 +0300

    Web > ClientSettings set

---
+++ Frontends/FreeCourse.Web/Models/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Web.Models
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Models/ClientSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Web.Models
+{
+    public class ClientSettings
+    {
+        public Client WebClient{ get; set; }
+        public Client WebClientForUser{ get; set; }
+    }
+}
