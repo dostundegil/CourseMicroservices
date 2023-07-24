542aa56615576b49434ecca36aa2da5784094ca2
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-25 00:38:47 +0300

    CoursesController created

---
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
