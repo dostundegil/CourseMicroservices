3420ae4522aab8700fc6453c6af302f8926a6d90
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-24 17:50:48 +0300

    AuthController > SignIn created

---
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
