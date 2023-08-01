4916e90db92f2e5d31e2e206092439f8699c2720
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-08-01 03:34:22 +0300

    IDiscountService / DiscountService created

---
+++ Frontends/FreeCourse.Web/Services/DiscountService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Web.Models.Discounts;
+using FreeCourse.Web.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class DiscountService : IDiscountService
+{
+    public Task<DiscountViewModel> GetDiscount(string discountCode)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+}
