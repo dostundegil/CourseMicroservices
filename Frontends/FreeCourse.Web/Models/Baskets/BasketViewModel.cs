8185cdf5305346e44c996ae65c50abc1b939b0f6
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-08-03 15:04:28 +0300

    bugs are fixed

--- Frontends/FreeCourse.Web/Models/Baskets/BasketViewModel.cs
+++ Frontends/FreeCourse.Web/Models/Baskets/BasketViewModel.cs
@@ -14,7 +14,7 @@ namespace FreeCourse.Web.Models.Baskets
         public string DiscountCode { get; set; }
 
         public int? DiscountRate { get; set; }
-        private List<BasketItemViewModel> _basketItems { get; set; }
+        private List<BasketItemViewModel> _basketItems;
 
         public List<BasketItemViewModel> BasketItems
         {
