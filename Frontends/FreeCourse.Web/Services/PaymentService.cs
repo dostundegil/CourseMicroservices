5d78ba00774d7f62196db4c19148cccb35ef670e
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-08-03 15:23:14 +0300

    IPaymentService && PaymentService set

---
+++ Frontends/FreeCourse.Web/Services/PaymentService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Web.Models.FakePayments;
+using FreeCourse.Web.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        
+        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
