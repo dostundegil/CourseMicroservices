c7f88df396670f672f32ba13e7b8455c703d9ce0
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-08-03 15:25:41 +0300

    PaymentService updated

--- Frontends/FreeCourse.Web/Services/PaymentService.cs
+++ Frontends/FreeCourse.Web/Services/PaymentService.cs
@@ -1,15 +1,26 @@
 ﻿using FreeCourse.Web.Models.FakePayments;
 using FreeCourse.Web.Services.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
 {
     public class PaymentService : IPaymentService
     {
-        
-        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
         {
-            throw new System.NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        {
+            var response = await _httpClient.PostAsJsonAsync<PaymentInfoInput>("fakepayments", paymentInfoInput);
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
