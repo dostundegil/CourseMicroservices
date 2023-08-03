323bdc4c7cbf4a488d3c9ad3d8344c79ef598d72
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-08-03 15:31:12 +0300

    CheckoutInfoInput set

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckoutInfoInput.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class CheckoutInfoInput
+    {
+        [Display(Name ="İl")]
+        public string Province { get; set; }
+
+        [Display(Name = "İlçe")]
+        public string District { get; set; }
+
+        [Display(Name = "Cadde")]
+        public string Street { get; set; }
+
+        [Display(Name = "Post kodu")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Adres")]
+        public string Line { get; set; }
+
+        [Display(Name = "Kart İsim - Soyisim")]
+        public string CardName { get; set; }
+
+        [Display(Name = "Kart Numarası")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "Son kullanma Tarihi (ay/yıl)")]
+        public string Expiration { get; set; }
+
+        [Display(Name = "CVV/CVV2 numarası")]
+        public string CVV { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
